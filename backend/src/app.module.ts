9316087bec9bfd8ced9c2fa55a167c9732674791
Michel Balvin Ormedo <michelbalvinor@gmail.com>
2024-02-20 12:05:26 -0500

    feat(Schema) Creacion de Schema de User

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 
 @Module({
   imports: [
-    
+    UserModule,
     MongooseModule.forRoot('mongodb://localhost:27017/smosGpt'),
     ],
  
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,44 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class UserService {
+
+  constructor(
+
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>
+  ){}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = await this.userModel.create( createUserDto)
+      return user;
+    } catch (error) {
+      if(error.code === 11000){
+        throw new BadRequestException(`Pokemon exists in BD ${JSON.stringify(error.keyValue)}`)
+      }
+      console.log(error);
+      throw new InternalServerErrorException(`Can't Create Pokemon -check server logs`)  
+
+    }
+  }
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
